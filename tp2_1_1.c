9e5139925678d2782fe688333cc9f379f3902ba6
ivan2214 <bongiovanniivan12@gmail.com>
2025-04-01 14:52:45 -0300

    fix : Arreglo del vscode que no tenia extension

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,16 +1,19 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define N 20
 
-int main(int argc, char const *argv[])
+int main()
 {
+    srand(time(NULL));
     int i;
     double vt[N];
     for (i = 0; i < N; i++)
     {
         vt[i] = 1 + rand() % 100;
-        printf('%f', vt[i]);
+        printf("%f \n", vt[i]);
     }
     return 0;
 }
